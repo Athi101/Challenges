8781b0d6f888834b81d987de41fbe3b3b7e2bb28
Varshininair <44626261+Varshininair@users.noreply.github.com>
2019-10-01 22:15:06 +0530

    Update PackagingCupcakes.java

--- codechef/PackagingCupcakes.java
+++ codechef/PackagingCupcakes.java
@@ -5,11 +5,11 @@ public class PackagingCupcakes{
  
 public static void main(String[] args) {
     Scanner ab = new Scanner(System.in);
-    int T = ab.nextInt();
-	while(T>0)  {
+    int C = ab.nextInt();
+	while(C>=1)  {
         int N = ab.nextInt();
         System.out.println((N/2)+1);
-        N = N-1;
+        N--;
     }
     
 }
