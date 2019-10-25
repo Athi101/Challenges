b48c7743792fc0771e8efdec737d9264c8fcb5a0
d3ku45 <53232960+d3ku45@users.noreply.github.com>
2019-10-25 10:51:06 +0530

    Update Fibonacci.java

--- codeforces/Fibonacci.java
+++ codeforces/Fibonacci.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class Fibonacci{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int k = sc.nextInt();
-        sc.close();
-        int a = 1, b =1, s=0 ,i=1;
+        Scanner s = new Scanner(System.in);
+        int k = s.nextInt();
+        s.close();
+        int a = 1, b =1, t=0 ,i=1;
         if(k==1){
             System.out.println(a);
         }
         
         else{
             while(i<=k){
-                s=a+b;
+                t=a+b;
                 a=b;
                 b=s;
                 i+=1;
             }
-            System.out.println(s-1);
+            System.out.println(t-1);
         }
         
     
-        
+      s.close();  
     }
 }
